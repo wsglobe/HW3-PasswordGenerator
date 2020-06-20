017fa28a668e8d4c2341c630058cfe945465eecf
Walter Wang <wsglobe@gmail.com>
2020-06-19 20:00:29 -0500

    js changed

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 //define global variables:
+var characterLength = 8;
+var userChoice = [];
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '()', '_', '+', '~', '`', '|', '}', '{', '[]', '\',': ','; ',' ? ',' >< ',', ','.',' / ',' - ','='];
 var lowerCaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-alert("Let's generate your password!");
-
 var generateBtn = document.querySelector("#generate");
-
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
-
+  
 generateBtn.addEventListener("click", writePassword);
 
+
+
+
+
+
+
 function generatePassword() {
-  alert("Let's generate your password!");
+  
   //ask what user like to use for their password
   var passwordLength = prompt("How many characters would you like your password to have? It must be between 8-128 characters");
   var confirmLowerCase = confirm("Would you like to use lowercase characters?");
@@ -41,3 +43,4 @@ function generatePassword() {
     alert("Choose again!");
   }
 }
+
